fd3d96e14f93908e8dd722da176544436198d55f
sriramnurani1 <sriram.ns.1995@gmail.com>
2015-08-06 10:46:45 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char * argv[])
+{
+if(argc<2)
+{
+fprintf(stdout,"Usage:%s number \n",argv[0]);
+return 1;
+}
+int v1= atoi(argv[1]);
+int v2= atoi(argv[2]);
+int v3;
+v3= v1+v2;
+fprintf(stdout,"The sum of %d and %d is %d\n",v1,v2,v3);
+return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char * argv[])
+{
+if(argc<2)
+{
+fprintf(stdout,"Usage:%s number \n",argv[0]);
+return 1;
+}
+int v1= atoi(argv[1]);
+int v2= atoi(argv[2]);
+int v3;
+v3= v1+v2;
+fprintf(stdout,"The sum of %d and %d is %d\n",v1,v2,v3);
+return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char * argv[])
+{
+if(argc<2)
+{
+fprintf(stdout,"Usage:%s number \n",argv[0]);
+return 1;
+}
+int v1= atoi(argv[1]);
+int v2= atoi(argv[2]);
+int v3;
+v3= v1+v2;
+fprintf(stdout,"The sum of %d and %d is %d\n",v1,v2,v3);
+return 0;
+}#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char * argv[])
+{
+if(argc<2)
+{
+fprintf(stdout,"Usage:%s number \n",argv[0]);
+return 1;
+}
+int v1= atoi(argv[1]);
+int v2= atoi(argv[2]);
+int v3;
+v3= v1+v2;
+fprintf(stdout,"The sum of %d and %d is %d\n",v1,v2,v3);
+return 0;
+}
